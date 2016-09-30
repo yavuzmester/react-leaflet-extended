00c9c311a09d169d327e488f7539211b67a185bc
yavuzmester <yavuzmester@gmail.com>
2016-09-30 08:32:21 +0300

    in progress

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const ReactLeaflet = require("react-leaflet");
 const Map = require("./src/map");
 const TileLayer = require("./src/tile-layer");
 const CanvasTileLayer = require("./src/canvas-tile-layer");
@@ -8,16 +7,11 @@ const GridLayer = require("./src/grid-layer");
 const HeatLayer = require("./src/heat-layer");
 const MarkerClusterGroupLayer = require("./src/marker-cluster-group-layer");
 
-Object.assign(
-    ReactLeaflet,
-    {
-        Map: Map,
-        TileLayer: TileLayer,
-        CanvasTileLayer: CanvasTileLayer,
-        GridLayer: GridLayer,
-        HeatLayer: HeatLayer,
-        MarkerClusterGroupLayer: MarkerClusterGroupLayer
-    }
-);
-
-module.exports = ReactLeaflet;
+module.exports = {
+    Map: Map,
+    TileLayer: TileLayer,
+    CanvasTileLayer: CanvasTileLayer,
+    GridLayer: GridLayer,
+    HeatLayer: HeatLayer,
+    MarkerClusterGroupLayer: MarkerClusterGroupLayer
+};
