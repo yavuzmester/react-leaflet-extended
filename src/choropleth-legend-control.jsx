10835b21894eacefcc069961d293c4b111e7544a
yavuzmester <yavuzmester@gmail.com>
2016-10-03 16:42:31 +0300

    in progress

--- src/choropleth-legend-control.jsx
+++ src/choropleth-legend-control.jsx
@@ -24,17 +24,22 @@ class ChoroplethLegendControl extends MapControl {
         const leafletElement = L.control(this.props);
 
         leafletElement.onAdd = function() {
-            const container = L.DomUtil.create("div", "geo-choropleth-legend  visibility-hidden");
+            const container = L.DomUtil.create("div", "geo-choropleth-legend visibility-hidden");
             leafletElement._container = container;
             return container;
         };
 
         this.leafletElement = leafletElement;
+
+        this.update();
     }
 
     componentDidUpdate(prevProps={} /*: object */) {
         super.componentDidUpdate(prevProps);
+        this.update();
+    }
 
+    update() {
         const {extents} = this.props;
 
         this.leafletElement._container.innerHTML = extents.reduce((memo, e) => {
