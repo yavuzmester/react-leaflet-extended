a7e85e0136392308a1c9f7ac194e3a5474ae031e
yavuzmester <yavuzmester@gmail.com>
2016-10-26 16:24:21 +0300

    babel applied before publish

---
+++ build/canvas-tile-layer.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const { PropTypes } = require("react");
+const RL_CanvasTileLayer = require("react-leaflet").CanvasTileLayer;
+const _ = require("underscore");
+
+class CanvasTileLayer extends RL_CanvasTileLayer {
+    tileCanvases() {
+        return _.values(this.leafletElement._tiles);
+    }
+}
+
+module.exports = CanvasTileLayer;
