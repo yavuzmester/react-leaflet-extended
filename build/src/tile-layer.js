f609ddd48caf627eb4da85a1e8a78ee667829ee8
yavuzmester <yavuzmester@gmail.com>
2016-10-27 09:00:37 +0300

    trying to prevent glitch on the tile layer at the very right side

--- build/src/tile-layer.js
+++ build/src/tile-layer.js
@@ -6,23 +6,30 @@ const _ = require("underscore");
 
 const propTypes = {
     name: PropTypes.string.isRequired,
-    noWrap: PropTypes.bool,
     url: PropTypes.string.isRequired,
     opacity: PropTypes.number
 };
 
 const defaultProps = {
-    noWrap: true,
     opacity: 1
 };
 
 class TileLayer extends RL_TileLayer {
+    constructor(props /*: object */, context /*: object */) {
+        super(props, context);
+        this.state({ noWrap: true }); //to prevent glitch on the tile layer at the very right side
+    }
+
     name() {
         return this.props.name;
     }
 
-    shouldComponentUpdate(nextProps /*: object */) /*: boolean */{
-        return !_.isEqual(_.pick(this.props, Object.keys(propTypes)), _.pick(nextProps, Object.keys(propTypes)));
+    componentDidMount() {
+        this.setState({ noWrap: false }); //to prevent glitch on the tile layer at the very right side
+    }
+
+    shouldComponentUpdate(nextProps /*: object */, nextState /*: object */) /*: boolean */{
+        return !_.isEqual(_.pick(this.props, Object.keys(propTypes)), _.pick(nextProps, Object.keys(propTypes))) || !_.isEqual(this.state, nextState);
     }
 }
 
--- src/tile-layer.jsx
+++ src/tile-layer.jsx
@@ -6,26 +6,33 @@ const _ = require("underscore");
 
 const propTypes = {
     name: PropTypes.string.isRequired,
-    noWrap: PropTypes.bool,
     url: PropTypes.string.isRequired,
     opacity: PropTypes.number
 };
 
 const defaultProps = {
-    noWrap: true,
     opacity: 1
 };
 
 class TileLayer extends RL_TileLayer {
+    constructor(props /*: object */, context /*: object */) {
+        super(props, context);
+        this.state({noWrap: true});      //to prevent glitch on the tile layer at the very right side
+    }
+
     name() {
         return this.props.name;
     }
 
-    shouldComponentUpdate(nextProps /*: object */) /*: boolean */ {
+    componentDidMount() {
+        this.setState({noWrap: false});  //to prevent glitch on the tile layer at the very right side
+    }
+
+    shouldComponentUpdate(nextProps /*: object */, nextState /*: object */) /*: boolean */ {
         return !_.isEqual(
             _.pick(this.props, Object.keys(propTypes)),
             _.pick(nextProps, Object.keys(propTypes))
-        );
+        ) || !_.isEqual(this.state, nextState);
     }
 }
 
