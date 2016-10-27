ed450647c5acd5da5ef44849432883153b036765
yavuzmester <yavuzmester@gmail.com>
2016-10-27 07:26:14 +0300

    trying to remove L.Map instance from state.

--- src/map.jsx
+++ src/map.jsx
@@ -2,11 +2,23 @@
 
 const RL_Map = require("react-leaflet").Map;
 const L = require("leaflet");
+const {omit, isUndefined} = require("underscore");
 
 class Map extends RL_Map {
+    componentDidMount () {
+        const props = omit(this.props, ["children", "className", "id", "style"]);
+        this.leafletElement = L.map(this.state.id, props);
+        super.componentDidMount();
+        //this.setState({map: this.leafletElement});    //commented out
+        if (!isUndefined(props.bounds)) {
+            this.leafletElement.fitBounds(props.bounds, props.boundsOptions);
+        }
+    }
+
     componentDidUpdate(prevProps /*: object */) {
         //Do nothing here, as it seems we are better without it.
     }
 }
 
 module.exports = Map;
+
