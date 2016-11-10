ebabfab3525e931227e96025a66bc64a4857d08a
yavuzmester <yavuzmester@gmail.com>
2016-11-10 09:01:39 +0200

    measure control added

---
+++ src/measure-control.jsx
@@ -0,0 +1,29 @@
+"use strict";
+
+const React = require("react");
+const {MapControl} = require("@yavuzmester/react-leaflet");
+const {control, DomUtil} = require("leaflet");
+require("leaflet.measure");
+const ReactDOM = require("react-dom");
+
+const defaultProps = {
+    position: "topleft"
+};
+
+class MeasureControl extends MapControl {
+    componentWillMount() {
+        const leafletElement = control.measure(this.props);
+
+        leafletElement.onAdd = function() {
+            const container = DomUtil.create("div", "geo-measure-control");
+            leafletElement._container = container;
+            return container;
+        };
+
+        this.leafletElement = leafletElement;
+    }
+}
+
+MeasureControl.defaultProps = defaultProps;
+
+module.exports = MeasureControl;
